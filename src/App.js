0eaf7646776c17ab8f1b6abb547e901c90e62928
Eduardo Molina <edrdmolina11@gmail.com>
2021-10-19 09:19:49 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+import Current from './components/Current';
+
 import './App.css';
 
+
 function App() {
+  const [lat, updateLat] = useState();
+  const [lon, updateLon] = useState();
+  const [units, changeUnit] = useState('imperial');
+  const [lang, changeLang] = useState('en')
+  const [isLoading, toggleIsLoading] = useState(true);
+  const [weatherData, updateWeatherData] = useState({});
+  // const [inputZip, toggleInputZip] = useState(false);
+
+  // Checks if location is supported by browser
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      console.log("Geo-Location is available");
+      getGeo()
+    } else {
+      console.log("Geo-Location is not available");
+    }
+
+    
+  }, [])
+
+  // Retrieves weather data from backend API.
+  // useEffect(() => {
+  //   async function getData() {
+  //     try {
+  //       const input = {
+  //         lat, lon, units, lang
+  //       }
+  //       const res = await axios.post('/api/weather', input)
+  //       console.log(res)
+  //       updateWeatherData(res.data);
+  //     } catch (err) {
+  //       console.log(err)
+  //     }
+  //   }  
+
+  //   if(!isLoading) {
+  //     setTimeout(() => {
+  //       getData();  
+  //     }, 1500);
+  //   }
+  // }, [isLoading, lat, lon, units, lang])
+  
+  function getGeo() {
+    navigator.geolocation.getCurrentPosition((p) => {
+      updateLat(p.coords.latitude);
+      updateLon(p.coords.longitude);
+      toggleIsLoading(false);
+    })
+  }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      < Current {...weatherData} />
     </div>
   );
 }
---
+++ src/components/Current.js
@@ -0,0 +1,11 @@
+function Current(props) {
+    return (
+        <div className='Current'>
+            <div className='current-temp'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Current;
