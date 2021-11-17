239f9ca4cebdd5ae19a6155fd09d7473d810eb18
Eduardo Molina <edrdmolina11@gmail.com>
2021-11-17 12:29:41 -0500

    Render Data on Current Component

--- src/App.js
+++ src/App.js
@@ -1,69 +1,63 @@
-import { useEffect, useState } from 'react';
-
+// Libraries
 import axios from 'axios';
-
+// Hooks
+import { useEffect, useState } from 'react';
+// Components
 import Current from './components/Current';
-
+// Styles
 import './App.css';
-
 
 function App() {
   const [lat, updateLat] = useState();
   const [lon, updateLon] = useState();
   const [units, changeUnit] = useState('imperial');
   const [lang, changeLang] = useState('en')
-  const [isLoading, toggleIsLoading] = useState(true);
   const [weatherData, updateWeatherData] = useState({});
+  const [locationData, updateLocationData] = useState({});
   // const [inputZip, toggleInputZip] = useState(false);
 
   // Checks if location is supported by browser
   useEffect(() => {
     if ("geolocation" in navigator) {
       console.log("Geo-Location is available");
+      // get geolocation coordinates
       getGeo()
+
+      async function getData() {
+        try {
+          const input = {
+            lat, lon, units, lang
+          }
+          const res = await axios.post('https://multi-purpose-api.herokuapp.com/api/weather', input)
+          updateLocationData(res.data.location[0]);
+          updateWeatherData(res.data.weatherData);
+        } catch (err) {
+          console.log(err)
+        }
+      }  
+      // 
+      if(lat && lon) getData();
     } else {
       console.log("Geo-Location is not available");
     }
+  }, [lat, lon, units, lang])
 
-    
-  }, [])
-
-  // Retrieves weather data from backend API.
-  // useEffect(() => {
-  //   async function getData() {
-  //     try {
-  //       const input = {
-  //         lat, lon, units, lang
-  //       }
-  //       const res = await axios.post('/api/weather', input)
-  //       console.log(res)
-  //       updateWeatherData(res.data);
-  //     } catch (err) {
-  //       console.log(err)
-  //     }
-  //   }  
-
-  //   if(!isLoading) {
-  //     setTimeout(() => {
-  //       getData();  
-  //     }, 1500);
-  //   }
-  // }, [isLoading, lat, lon, units, lang])
-  
+  // Get geolocation
   function getGeo() {
     navigator.geolocation.getCurrentPosition((p) => {
       updateLat(p.coords.latitude);
       updateLon(p.coords.longitude);
-      toggleIsLoading(false);
     })
   }
   
-  
+  console.log(weatherData)
+
   return (
     <div className="App">
-      < Current {...weatherData} />
+      < Current {...weatherData} locationData={locationData} />
     </div>
   );
+  
 }
 
 export default App;
--- src/components/Current.js
+++ src/components/Current.js
@@ -1,8 +1,31 @@
 function Current(props) {
+    const { locationData, current, daily } = props
+    
+    let weather = 'N/A';
+    let min = 'N/A';
+    let max = 'N/A';
+    if(current) weather = current.weather[0].main;
+    if(daily) {
+        min = Math.abs(parseInt(daily[0].temp.min));
+        max = Math.abs(parseInt(daily[0].temp.max));
+    }
+
     return (
         <div className='Current'>
+            <div className='current-location'>
+                { locationData ? locationData.city : 'N/A' }
+            </div>
             <div className='current-temp'>
-                
+                { current ? Math.abs(parseInt(current.temp)) : 'N/A'}
+            </div>
+            <div className='current-weather'>
+                { weather }
+            </div>
+            <div className='current-min'>
+                {min}
+            </div>
+            <div className='current-max'>
+                {max}
             </div>
         </div>
     )
